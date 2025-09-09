8d3e84217e9b92cad42d4686987f3e2d316cf05e
Monarch-Codes00 <chinedumroster@gmail.com>
2025-09-10 08:51:32 +0930

    `Added PetOwnerPage component with userName prop and basic layout`

--- react/src/pages/Homepage/PetOwnerPage.jsx
+++ react/src/pages/Homepage/PetOwnerPage.jsx
@@ -2,6 +2,21 @@ import LocationTicker from '../../components/LocationTicker';
 import AboutUs from '../../components/PetOwner/ContactUs/AboutUs';
 import ContactUs from '../../components/PetOwner/ContactUs/ContactUs';
 
-
+const PetOwnerPage = ({ userName }) => {
+  return (
+    <div className="pet-owner-page">
+      <h1>Welcome, {userName}!</h1>
+      {/* <LocationTicker /> */}
+      <section>
+        {/* <h2>About Us</h2> */}
+        <AboutUs />
+      </section>
+      <section>
+        <h2>Contact Us</h2>
+        <ContactUs />
+      </section>
+    </div>
+  );
+};
 
 export default PetOwnerPage;
