d27809f7600a96919551afcc6bf1352aa2d46caf
MichaeL Enoch <mike.saviour08@gmail.com>
2025-09-10 11:47:07 +0100

    updated

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../styles/NavBar.css';
+import { Link } from 'react-router-dom';
+import './Nav.css';
 
-export default function Nav() {
+const Nav = ({ userCategory }) => {
   return (
-    <header className="navbar">
-      <div className="navbar-left">
-        <div className="brand">FurEver Care</div>
+    <nav className="navbar">
+      <div className="nav-logo">
+        <Link to="/">FurEver Care</Link>
       </div>
-      <nav className="navbar-right">
-        <NavLink to="/" end className={({ isActive }) => isActive ? 'navlink active' : 'navlink'}>
-          Home
-        </NavLink>
-        <NavLink to="/about" className={({ isActive }) => isActive ? 'navlink active' : 'navlink'}>
-          About
-        </NavLink>
-        <NavLink to="/contact" className={({ isActive }) => isActive ? 'navlink active' : 'navlink'}>
-          Contact
-        </NavLink>
-      </nav>
-    </header>
+      <ul className="nav-menu">
+        <li><Link to="/">Home</Link></li>
+        {userCategory === 'Pet Owner' && (
+          <>
+            <li><Link to="/Pet-Care">Pet Care</Link></li>
+            <li><Link to="/pet-owner">Products</Link></li>
+            <li><Link to="/pet-owner">Emergency</Link></li>
+            <li><Link to="/pet-owner">Feedback</Link></li>
+            <li><Link to="/pet-owner">Contact Us</Link></li>
+          </>
+        )}
+        {userCategory === 'Veterinarian' && (
+          <>
+            <li><Link to="/veterinarian">Profile</Link></li>
+            <li><Link to="/veterinarian">Appointments</Link></li>
+            <li><Link to="/veterinarian">Medical Histories</Link></li>
+          </>
+        )}
+        {userCategory === 'Animal Shelter' && (
+          <>
+            <li><Link to="/shelter">Events</Link></li>
+            <li><Link to="/shelter">Gallery</Link></li>
+            <li><Link to="/shelter">Shelter Contact</Link></li>
+            <li><Link to="/shelter">Success Stories</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
-}
+};
+
+export default Nav;
