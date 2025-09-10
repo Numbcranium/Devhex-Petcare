569bd86efb5cc2c4619a78a430d2025d4976b43b
Monarch-Codes00 <chinedumroster@gmail.com>
2025-09-10 13:37:27 +0930

    `Added dynamic section rendering based on URL query parameter 'section' to PetOwnerPage component.`

--- react/src/pages/Homepage/PetOwnerPage.jsx
+++ react/src/pages/Homepage/PetOwnerPage.jsx
@@ -1,17 +1,24 @@
+import { useLocation } from 'react-router-dom';
 import LocationTicker from '../../components/LocationTicker';
 import AboutUs from '../../components/PetOwner/ContactUs/AboutUs';
 import ContactUs from '../../components/PetOwner/ContactUs/ContactUs';
 
 const PetOwnerPage = ({ userName }) => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const section = queryParams.get('section');
+
   return (
     <div className="pet-owner-page">
       <h1>Welcome, {userName}!</h1>
       {/* <LocationTicker /> */}
       <section>
         {/* <h2>About Us</h2> */}
-        <AboutUs />
+        {section === 'contact-us' && <AboutUs />}
+        {section === 'products' && <div>Products Section Content</div>}
+        {section === 'emergency' && <div>Emergency Section Content</div>}
+        {section === 'feedback' && <div>Feedback Section Content</div>}
       </section>
-    
     </div>
   );
 };
